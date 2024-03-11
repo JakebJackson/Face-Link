dba31ce0dbda2d7060f43915d8e6ebd7dc03d10c
Mark Lewis <mlewis89@gmail.com>
2024-03-11 18:24:13 +1100

    basic routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//const ApiRoutes = require('./api');
-//router.use('/api',ApiRoutes);
+const ApiRoutes = require('./api');
+router.use('/api',ApiRoutes);
 
 module.exports = router
