92d8a688fc8ab2582c440cd0ef87ecb8874749e4
Mark Lewis <mlewis89@gmail.com>
2024-03-11 17:47:33 +1100

    basic server.js

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 //External Resources
 const express = require('express');
-const session =  requiw('express-session)');
-const sequelize = require('sequelize');
-const SequelizeStore =  require('connect-session-sequelise')(session.Store);
 
 //internal Resources
 const routes = require('./controllers');
 
-//Port Config
+
+//intances and Port Config
 const app = express();
 const PORT =  process.env.PORT || 3001;
 
-const sess =  {
-    secret =  process.env.SESS_SECRET,
-    cookie : {
-        maxAge: (1*1000*60*60),
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
-    resave: false,
-    saveUnitiatlised: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-app.use(session(sess));
-
+//Middle wear
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
