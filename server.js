6520a13a8036ac169c09a7f743fcc6e338fb73ab
Mark Lewis <mlewis89@gmail.com>
2024-03-11 18:03:43 +1100

    basic connection.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require('express');
 
 //internal Resources
+const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
 
 //intances and Port Config
 const app = express();
 const PORT =  process.env.PORT || 3001;
+
 
 //Middle wear
 app.use(express.json());
