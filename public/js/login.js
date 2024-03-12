32ccd1e7d272bc7d32b799c048077b004056c685
David2003Park <151422154+David2003Park@users.noreply.github.com>
2024-03-12 21:28:21 +1100

    Update login.js
    changed signup to login

--- public/js/login.js
+++ public/js/login.js
@@ -25,12 +25,12 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#name-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if (name && email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
