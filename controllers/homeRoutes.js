5692df9b8942d992c1a7bd094d9371cc10be4572
Mark Lewis <mlewis89@gmail.com>
2024-03-18 10:04:24 +1100

    user images render to dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User , Images, Faces} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-      const userData = await User.findAll({
+      console.log(req.session.user_i);
+
+      const userData = await User.findByPk(req.session.user_id,{
+        include: [{ model: Images }, { model: Faces }],
         attributes: { exclude: ['password'] },
-        order: [['username', 'ASC']],
       });
-  
-      const users = userData.map((project) => project.get({ plain: true }));
-  
+
+      const user = userData.get({ plain: true });
+      
       res.render('dashboard', {
-        users,
+        user,
         logged_in: req.session.logged_in,
       });
     } catch (err) {
