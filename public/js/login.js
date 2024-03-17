e7693470acf4aa26fae0dc7191e10ea392c665bc
Mark Lewis <mlewis89@gmail.com>
2024-03-17 17:30:06 +1100

    relocated the session.save to inside try block

--- public/js/login.js
+++ public/js/login.js
@@ -21,20 +21,20 @@ const loginFormHandler = async (event) => {
       }
     }
   };
-  
-  const signupFormHandler = async (event) => {
+
+const signupFormHandler = async (event) => {
     event.preventDefault();
   
-const username = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+        const response = await fetch('/api/users/signup', {
+          method: 'POST',
+          body: JSON.stringify({ username, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+          });
   
       if (response.ok) {
         document.location.replace('/');
