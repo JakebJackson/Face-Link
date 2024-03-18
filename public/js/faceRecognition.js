1a5e3537ad9d5a68920a26c38bf9eea950bc888c
Mark Lewis <mlewis89@gmail.com>
2024-03-18 11:59:24 +1100

    include face recognition on front end

---
+++ public/js/faceRecognition.js
@@ -0,0 +1,27 @@
+//const faceapi = require('face-api.js');
+
+
+
+
+
+const getFaceDataforHTMLElement = async (input)=>
+{
+  const Model =  await faceapi.nets.tinyFaceDetector.loadFromUri('./faceModels');  
+    
+  const detections1 = await faceapi.detectAllFaces(input, new faceapi.SsdMobilenetv1Options())
+  const detections2 = await faceapi.detectAllFaces(input, new faceapi.TinyFaceDetectorOptions())
+    console.log(detections1, detections2);
+}
+
+const storeFaceToDB = (faceData)=>
+{
+
+}
+
+const findFacesInImages = (faceIDArr,ImgArr) =>
+{
+
+}
+
+const input = document.getElementById('imageID-5');
+getFaceDataforHTMLElement(input);
